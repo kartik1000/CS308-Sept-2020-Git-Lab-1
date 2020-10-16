5c214f8c13769d8756b0f8a3aea58a812abaff21
Kartik Kathuria <kathuriakartik0@gmail.com>
2020-10-16 16:59:20 +0530

    added debug prefix statement

--- sqrt.c
+++ sqrt.c
@@ -10,19 +10,19 @@ positive number. */
 
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
-		printf("NEGSUPPORT: Usage: sqrt input\n\n");
+		printf("DEBUG: Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
     if(input<0){
-       printf("NEGSUPPORT: Sqrt of given number /n");
-       printf("NEGSUPPORT: Sqrt of %d is %f i \n",input,sqrt(input));
+       printf("DEBUG: Sqrt of given number /n");
+       printf("DEBUG: Sqrt of %d is %f i \n",input,sqrt(input));
         
     }
     else{
-       printf("NEGSUPPORT: Sqrt of given number /n");
-	   printf("NEGSUPPORT: Sqrt of %d is %f\n",input,sqrt(input));
+       printf("DEBUG: Sqrt of given number /n");
+	   printf("DEBUG: Sqrt of %d is %f\n",input,sqrt(input));
     }
     printf("NEGSUPPORT: End of Program existing");
 	return(0);
