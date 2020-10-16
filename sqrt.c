de3c1663fa4574b7f4aeea600998cd76039d29f1
Kartik Kathuria <kathuriakartik0@gmail.com>
2020-10-16 17:25:15 +0530

    Update sqrt.c

--- sqrt.c
+++ sqrt.c
@@ -11,36 +11,21 @@ positive number. */
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
 		printf("Usage: sqrt input\n\n");
-		printf("NEGSUPPORT: Usage: sqrt input\n\n");
-		printf("DEBUG: Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
     if(input<0){
         printf("Square root of negative numbers \n");
-        printf("NEGSUPPORT: Sqrt of given number /n");
-        printf("DEBUG: Square root of negative numbers \n");
         printf("Sqrt of given number /n");
-        printf("NEGSUPPORT: Sqrt of given number /n");
-        printf("DEBUG: Sqrt of given number /n");
         printf("Sqrt of %d is %f i \n",input,sqrt(input));
-	printf("NEGSUPPORT: Sqrt of %d is %f i \n",input,sqrt(input))
-	printf("DEBUG: Sqrt of %d is %f i \n",input,sqrt(input));
         
     }
     else{
         printf("Square root of positive  numbers \n");
-	printf("NEGSUPPORT: Square root of positive  numbers \n")
-	printf("DEBUG: Square root of positive  numbers \n")
         printf("Sqrt of given number /n");
-	printf("NEGSUPPORT: Sqrt of given number /n");
-	printf("DEBUG: Sqrt of given number /n");
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
-	printf("NEGSUPPORT: Sqrt of %d is %f\n",input,sqrt(input));
-	printf("DEBUG: Sqrt of %d is %f\n",input,sqrt(input));
     }
-    printf("NEGSUPPORT: End of Program existing");
     printf("End of Program existing");
     return(0);
 
